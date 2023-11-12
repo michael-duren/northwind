db0aeed1b8f495c3b4a78b2ac09da89c4999a753
Michael Duren <michaeld@michaelduren.com>
2023-11-12 06:54:48 -0600

    add razor page lib classes

---
+++ RazorClassLib/Areas/MyFeature/Pages/EmployeesList.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using DataContext;
+using Domain;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorClassLib.Areas.MyFeature.Pages;
+
+public class EmployeesListModel : PageModel
+{
+    private readonly NorthwindContext _context;
+
+    public EmployeesListModel(NorthwindContext context)
+    {
+        _context = context;
+    }
+
+    public Employee[] Employees { get; set; } = null!;
+
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind B2B - Employees";
+
+        Employees = _context.Employees.OrderBy(e => e.LastName)
+            .ThenBy(e => e.FirstName).ToArray();
+    }
+}
